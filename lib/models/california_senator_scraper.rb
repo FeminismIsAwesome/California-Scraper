c50eadbd9a50a29d9dcb807833d5e7def83bdf41
Ian Norris <ian.norris.1991@gmail.com>
2014-12-11 00:59:39 -0800

    adding senators

---
+++ lib/models/california_senator_scraper.rb
@@ -0,0 +1,48 @@
+class CaliforniaSenatorScraper
+  @@senate_reference_url = "http://senate.ca.gov/senators"
+  def self.getCaliforniaSenators
+    response = RestClient.get @@senate_reference_url
+    html_doc = Nokogiri::HTML(response.body)
+    rows = html_doc.css(".views-row")
+    rows.map {|row|
+      name = row.css("h3")
+      unless name.text.include?("Vacant")
+        details = parse_name_and_party_from(name.text)
+        details[:state] = "CA"
+        details[:house] = "Senator"
+        details[:district] = row.css(".views-field-field-senator-district").text.strip
+        details[:email] = row.css(".views-field-field-senator-feedbackurl a").first[:href]
+        details[:capital_phone] = row.css(".views-field-field-senator-capitol-office p").text.match(/\(\d\d\d\) \d\d\d-\d\d\d\d/)[0]
+        Legislator.new(details)
+      else
+        nil
+      end
+    }.select {|row|
+      row.present?
+    }
+  end
+
+  def self.parse_name_and_party_from text
+    space_seperated = text.split(" ")
+    first_name = space_seperated[0]
+    last_name = space_seperated[1]
+    middle_name = nil
+    party_affiliation = space_seperated[2]
+    if has_middle_name(space_seperated)
+      middle_name = space_seperated[1]
+      last_name = space_seperated[2]
+      party_affiliation = space_seperated[3]
+    end
+    party_affiliation = party_affiliation.gsub("(","").gsub(")","")
+    senator = Hash.new
+    senator[:first_name] = first_name
+    senator[:last_name] = last_name
+    senator[:party] = party_affiliation
+    senator[:middle_name] = middle_name
+    senator
+  end
+
+  def self.has_middle_name(space_seperated_text)
+    space_seperated_text.length == 4
+  end
+end
---
+++ spec/integration/california_senator_scraper_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe CaliforniaAssemblyLegislatureScraper, :type => :model do
+
+  it "should get names of all members in senator roster" do
+    contents = File.open("spec/integration/examples/senators_and_assembly/senators.html").read
+    net_http_resp = Net::HTTPResponse.new(1.0, 200, "OK")
+    my_response = RestClient::Response.create(contents, net_http_resp, nil)
+    expect(RestClient).to receive(:get) {
+      my_response
+    }
+    legislators = CaliforniaSenatorScraper.getCaliforniaSenators()
+    expect(legislators.length).to eq(39)
+  end
+
+  it "should get first senator's detail in roster" do
+    contents = File.open("spec/integration/examples/senators_and_assembly/senators.html").read
+    net_http_resp = Net::HTTPResponse.new(1.0, 200, "OK")
+    my_response = RestClient::Response.create(contents, net_http_resp, nil)
+    expect(RestClient).to receive(:get) {
+      my_response
+    }
+    legislators = CaliforniaSenatorScraper.getCaliforniaSenators()
+    firstLegislator = legislators[0]
+    expect(firstLegislator.first_name).to eq("Benjamin")
+    expect(firstLegislator.last_name).to eq("Allen")
+    expect(firstLegislator.middle_name).to be_nil
+    expect(firstLegislator.state).to eq("CA")
+    expect(firstLegislator.house).to eq("Senator")
+    expect(firstLegislator.district).to eq("District 26")
+    expect(firstLegislator.email).to eq("https://lcmspubcontact.lc.ca.gov/PublicLCMS/ContactPopup.php?district=SD26")
+    expect(firstLegislator.capital_phone).to eq("(916) 651-4026")
+  end
+end
