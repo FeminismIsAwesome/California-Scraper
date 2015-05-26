20070ccfe1873a29345e0da4d9f601cc2184f37b
Ian Norris <ian.norris.1991@gmail.com>
2015-05-25 17:12:57 -0700

    Adding unit test for angular

--- spec/javascripts/controllers/ReportCardController_spec.js
+++ spec/javascripts/controllers/ReportCardController_spec.js
@@ -1,6 +1,13 @@
 describe("RecipesController", function() {
+	beforeEach(module('reportcard'));
+	controller = undefined;
+	scope = {};
+	beforeEach(inject(function($rootScope, $controller) {
+		scope = $rootScope.$new();
+		controller = $controller('ReportCardHomeController', {$scope: scope});
+	}));
 	it("should run a test for me", function() {
 		expect(2).toBe(2);
-		// expect(1).toBe(2);
+		expect(scope.greeting).toBe("Hola! This is Report Card Builder v0.1");
 	});
 });
