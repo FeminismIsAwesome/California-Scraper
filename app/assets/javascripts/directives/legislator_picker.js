a9b688bd5b4da587eb4d75bf66a4735fec83bee1
Ian Norris <ian.norris.1991@gmail.com>
2015-08-19 18:40:40 -0700

    more tests

--- app/assets/javascripts/directives/legislator_picker.js
+++ app/assets/javascripts/directives/legislator_picker.js
@@ -1,35 +1,35 @@
 app = angular.module('reportcard');
 console.log("No legislator picker loads first");
-app.directive('legislatorNameSearchbar',function(LegislatorDataService) {
-  return {
-    transclude: true,
-    templateUrl: '/templates/legislator_search.html',
-    scope: {
-      selectedLegislator: '='
-    },
-    link: function(scope,elem, attrs) {
-      LegislatorDataService.getLegislators().then(function(legislators) {
-        scope.legislators = legislators.data.map(function(legislator) {
-            legislator.fullName = legislator.first_name + " " + legislator.last_name;
-            return legislator;
-        }).sort(function(legislator1, legislator2) {
-          return legislator1.fullName.localeCompare(legislator2.fullName);
-        });
-        scope.legislatorsToSearch = scope.legislators;
-      });
-      scope.selectLegislator = function($index) {
-        scope.selectedLegislatorIndex = $index;
-        scope.selectedLegislator = scope.legislatorsToSearch[$index];
-      }
-      scope.shouldHighlight = function($index) {
-        return scope.selectedLegislatorIndex === $index;
-      }
-    	scope.searchForLegislators = function() {
-    		scope.legislatorsToSearch = scope.legislators.filter(function(legislator) {
-        	return legislator.fullName.includes(scope.legislatorSearch);
-    		});
-  		}
-  	}
+// app.directive('legislatorNameSearchbar',function(LegislatorDataService) {
+//   return {
+//     transclude: true,
+//     templateUrl: '/templates/legislator_search.html',
+//     scope: {
+//       selectedLegislator: '='
+//     },
+//     link: function(scope,elem, attrs) {
+//       LegislatorDataService.getLegislators().then(function(legislators) {
+//         scope.legislators = legislators.data.map(function(legislator) {
+//             legislator.fullName = legislator.first_name + " " + legislator.last_name;
+//             return legislator;
+//         }).sort(function(legislator1, legislator2) {
+//           return legislator1.fullName.localeCompare(legislator2.fullName);
+//         });
+//         scope.legislatorsToSearch = scope.legislators;
+//       });
+//       scope.selectLegislator = function($index) {
+//         scope.selectedLegislatorIndex = $index;
+//         scope.selectedLegislator = scope.legislatorsToSearch[$index];
+//       }
+//       scope.shouldHighlight = function($index) {
+//         return scope.selectedLegislatorIndex === $index;
+//       }
+//     	scope.searchForLegislators = function() {
+//     		scope.legislatorsToSearch = scope.legislators.filter(function(legislator) {
+//         	return legislator.fullName.includes(scope.legislatorSearch);
+//     		});
+//   		}
+//   	}
 
-  }
-});
+//   }
+// });
