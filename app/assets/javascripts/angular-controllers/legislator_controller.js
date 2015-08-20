15ffe7b952e5a895bbdcfbb7d921af56debb0a4d
Ian Norris <ian.norris.1991@gmail.com>
2015-08-19 21:20:00 -0700

    refactored legislator controller to be a little more sane

--- app/assets/javascripts/angular-controllers/legislator_controller.js
+++ app/assets/javascripts/angular-controllers/legislator_controller.js
@@ -10,7 +10,62 @@ function sortBills(bills) {
             }
         });
 }
+function sortByLastName(legislatorWithVote, otherLegislatorWithVote) {
+    var legislatorName = legislatorWithVote.legislator.last_name;
+    var otherLegislatorName = otherLegislatorWithVote.legislator.last_name;
+    return legislatorName.localeCompare(otherLegislatorName);
+}
+function getMissingVotesGiven(bills, votes) {
+    var missingBillVotes = bills.filter(function(vote) {
+        var possiblyHavingBillVote = votes.filter(function(targetVote) {
+            return targetVote.bill_number === vote.billNumber && targetVote.bill_type === vote.billType;
+        });
+        return possiblyHavingBillVote.length === 0;
+    });
+    missingBillVotes = missingBillVotes.map(function(billVote) {
+        billVote.notAccountedFor = true;
+        return billVote;
+    });
+    return missingBillVotes;
+}
+function parseVotes(votes) {
+    return votes.map(function(vote) {
+        vote.billNumber = vote.billNumber || vote.bill_number;
+        vote.billType = vote.billType || vote.bill_type;
+        return vote;
+    })
+}
+function removeDuplicates(votes) {
+    var billAndType = {};
+    votes.forEach(function(vote) {
+        if(billAndType[vote.bill_number + vote.bill_type]) {
+            bundledVote = {voting_location: vote.voting_location, vote: vote.vote}
+            var packagedVote = billAndType[vote.bill_number + vote.bill_type];
+            packagedVote.bundledVotes = packagedVote.bundledVotes.concat([bundledVote]);
+            billAndType[vote.bill_number + vote.bill_type] = packagedVote;
+        }
+        else {
+            bundledVote = {voting_location: vote.voting_location, vote: vote.vote}
+            vote.bundledVotes = [bundledVote];
+            billAndType[vote.bill_number + vote.bill_type] = vote;
+        }
+    });
+    var newVotes = [];
+    Object.keys(billAndType).forEach(function(billIdentifier) {
+        newVotes.push(billAndType[billIdentifier]);
+    });
+    return newVotes;
+}
 app.controller('LegislatorsController', ['$scope', 'LegislatorDataService','BillService', function($scope, LegislatorDataService,BillService) {
+    function organizeVotes(legislatorWithVote) {
+        var missingBillVotes = getMissingVotesGiven($scope.bills, legislatorWithVote.votes);
+        var billAndType = {};
+        legislatorWithVote.votes = removeDuplicates(legislatorWithVote.votes);
+        legislatorWithVote.votes = parseVotes(missingBillVotes.concat(legislatorWithVote.votes));
+        legislatorWithVote.votes = sortBills(legislatorWithVote.votes);
+        return legislatorWithVote;
+    }
+
     LegislatorDataService.getLegislators().then(function(legislators) {
         return legislators.data.map(function(legislator) {
             legislator.fullName = legislator.first_name + " " + legislator.last_name;
@@ -21,48 +76,7 @@ app.controller('LegislatorsController', ['$scope', 'LegislatorDataService','Bill
         var bills = BillService.getBillsForCard();
         $scope.bills = sortBills(bills);
         return LegislatorDataService.getBillsForAllLegislators(bills).then(function(votes) {
-            $scope.legislatorsWithVotes = votes.data.sort(function(legislatorWithVote, otherLegislatorWithVote) {
-                var legislatorName = legislatorWithVote.legislator.last_name;
-                var otherLegislatorName = otherLegislatorWithVote.legislator.last_name;
-                return legislatorName.localeCompare(otherLegislatorName);
-            }).map(function(legislatorWithVote) {
-                var missingBillVotes = $scope.bills.filter(function(vote) {
-                    var possiblyHavingBillVote = legislatorWithVote.votes.filter(function(targetVote) {
-                        return targetVote.bill_number === vote.billNumber && targetVote.bill_type === vote.billType;
-                    });
-                    return possiblyHavingBillVote.length === 0;
-                });
-                missingBillVotes = missingBillVotes.map(function(billVote) {
-                    billVote.notAccountedFor = true;
-                    return billVote;
-                });
-                var billAndType = {};
-                legislatorWithVote.votes.forEach(function(vote) {
-                    if(billAndType[vote.bill_number + vote.bill_type]) {
-                        bundledVote = {voting_location: vote.voting_location, vote: vote.vote}
-                        var packagedVote = billAndType[vote.bill_number + vote.bill_type];
-                        packagedVote.bundledVotes = packagedVote.bundledVotes.concat([bundledVote]);
-                        billAndType[vote.bill_number + vote.bill_type] = packagedVote;
-                    }
-                    else {
-                        bundledVote = {voting_location: vote.voting_location, vote: vote.vote}
-                        vote.bundledVotes = [bundledVote];
-                        billAndType[vote.bill_number + vote.bill_type] = vote;
-                    }
-                });
-                var newVotes = [];
-                Object.keys(billAndType).forEach(function(billIdentifier) {
-                    newVotes.push(billAndType[billIdentifier]);
-                });
-                legislatorWithVote.votes = newVotes;
-                legislatorWithVote.votes = missingBillVotes.concat(legislatorWithVote.votes).map(function(vote) {
-                        vote.billNumber = vote.billNumber || vote.bill_number;
-                        vote.billType = vote.billType || vote.bill_type;
-                        return vote;
-                    })
-                legislatorWithVote.votes = sortBills(legislatorWithVote.votes);
-                return legislatorWithVote;
-            });
+            $scope.legislatorsWithVotes = votes.data.sort(sortByLastName).map(organizeVotes);
         });
     });
 }]);
