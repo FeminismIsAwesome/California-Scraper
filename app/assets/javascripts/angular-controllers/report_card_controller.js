b251085dbf6c5ed3474673f68e7fd848e456d574
Ian Norris <ian.norris.1991@gmail.com>
2015-08-21 08:53:23 -0700

    adding grades

--- app/assets/javascripts/angular-controllers/report_card_controller.js
+++ app/assets/javascripts/angular-controllers/report_card_controller.js
@@ -5,9 +5,32 @@ app.controller('ReportCardHomeController', ['$scope', 'LegislatorDataService', '
       var bills = BillService.getBillsForCard();
       LegislatorDataService.getBillsForLegislator(bills, legislator).then(function(votes) {
         $scope.votes = votes.data;
+        $scope.voteTotal = $scope.votes.reduce(function(memo, vote) {
+          var voteWeight = 0;
+          if (vote.vote === "ayes") {
+            voteWeight = 1;
+          } else if (vote.vote === "noes") {
+            voteWeight = -1;
+          }
+          return memo + voteWeight;
+        }, 0);
       });
     }
   });
+  $scope.renderVoteAsGrade = function() {
+    var percentage = $scope.voteTotal / $scope.votes.length;
+    if (percentage >= 0.9) {
+      return "A";
+    } else if (percentage >= 0.8) {
+      return "B";
+    } else if (percentage >= 0.7) {
+      return "C";
+    } else if (percentage >= 0.6) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
   $scope.goToAllLegislators = function() {
     $location.path('/reportCard/all-legislators');
   }
